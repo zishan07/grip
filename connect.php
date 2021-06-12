093b021fac896a99410ab0b7087e59dcdc230d13
zishan07 <67476644+zishan07@users.noreply.github.com>
2021-06-12 21:10:14 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,10 +1,10 @@
 <?php
 	//Connection
 
-	$servername = "sql304.epizy.com";
-	$username = "epiz_28651824";
-	$password = "Ew1I8YyPV9OI";
-	$dbname = "epiz_28651824_gripattsf";
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "banking";
 
 	$db=mysqli_connect($servername , $username , $password , $dbname) or die('Cannot connect to server'.mysqli_connect_error());
 	
